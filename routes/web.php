b38ba2a7e08dbf758520bd4715c86e6c6027cdee
gedha rizka <gedharizka@gmail.com>
2020-03-28 22:03:14 +0700

    Product index

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'DashboardController@index');
+Route::get('/', 'DashboardController@index')->name('dashboard');
 Auth::routes(); //register enable
 // Auth::routes(['register' => false]); //register disable
+
+Route::resource('products', 'ProductController');
